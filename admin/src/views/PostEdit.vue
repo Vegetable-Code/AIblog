a96bcc88c3ce207cc4f5b1dc11fdd3e6ce80d98b
Blog Deploy <deploy@blog.com>
2026-07-18 17:56:51 +0800

    feat: 代码块语法高亮，使用 marked-highlight + highlight.js

--- admin/src/views/PostEdit.vue
+++ admin/src/views/PostEdit.vue
@@ -117,6 +117,18 @@ import { ElMessage } from 'element-plus'
 import { Plus } from '@element-plus/icons-vue'
 import { api } from '../stores/auth'
 import { marked } from 'marked'
+import { markedHighlight } from 'marked-highlight'
+import hljs from 'highlight.js'
+
+marked.use(markedHighlight({
+  langPrefix: 'hljs language-',
+  highlight(code, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      return hljs.highlight(code, { language: lang }).value
+    }
+    return hljs.highlightAuto(code).value
+  }
+}))
 
 const route = useRoute()
 const router = useRouter()
